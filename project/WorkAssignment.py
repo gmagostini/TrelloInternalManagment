2856d1810d7f0648d36681c8b20ccd3ff381386b
Giulio Aogostini <fizban94@gmail.com>
2021-02-13 17:55:42 +0100

    beta compleata
    scazione delle checklist e creazione delle card completata \n manca tutta la parte di cosa succede se si cancella qualcosa \n e come gestire bene gli utenti

--- project/WorkAssignment.py
+++ project/WorkAssignment.py
@@ -8,19 +8,31 @@ import re
 class WorkAssigment:
     checklist = {}
     def __init__(self):
-        self.read_key()
-        self.read_checklist()
-        self.query = {
-            'key': self.key['api_key'],
-            'token': self.key['admin_token']
-        }
+        i = 0
+        while True:
+            print("===================================================================================================")
+            print(f"\t\t\t\tINIZIO CICLIO : {i}")
+            print("===================================================================================================")
+            self.read_key()
+            self.read_checklist()
+            self.query = {
+                'key': self.key['api_key'],
+                'token': self.key['admin_token']
+            }
 
-        self.gett_all_checklist()
-        self.check_managment()
-        self.create_card()
-        self.write_checklist()
-        self.order_list()
-        self.write_key()
+            self.gett_all_checklist()
+            self.check_managment()
+            self.create_card()
+            self.order_list()
+
+
+            self.write_checklist()
+            self.write_key()
+            time.sleep(10)
+            print("===================================================================================================")
+            print(f"\t\t\t\tFINE CICLIO : {i}")
+            print("===================================================================================================")
+            i += 1
 
     def read_key(self):
         """Leggi il dizzionario [self.key]"""
@@ -59,14 +71,29 @@ class WorkAssigment:
             )
 
             if response.__str__() == "<Response [200]>":
-                for elemento in response.json():
-                    if elemento['id'] not in self.checklist.keys():
-                        list = []
-                        print(f"[NEW]{elemento['id']}\t{elemento['name']}")
-                        for linea in elemento['checkItems']:
-                            list.append({'id':linea['id'],'name':linea['name'],'due':linea['due'],'member':linea['idMember']})
-                            print(f"\t\t{linea['id']}\t{linea['name']}")
-                        self.checklist[elemento['id']] = {"title" : elemento['name'], 'urlCard':self.get_card_link(elemento['idCard']), "element":list}
+                for trello_checklist in response.json():
+
+                    if trello_checklist['id'] not in self.checklist.keys(): #Controlla se la cheklist sia gia stata salvata
+
+                        if self.chek_card_status(trello_checklist['idCard']) == False: # Se no controlla che non sia in una card chiuse
+                            list = {}
+                            print(f"[NEW]{trello_checklist['id']}\t{trello_checklist['name']}")
+                            for linea in trello_checklist['checkItems']:
+                                list[linea['id']] = {'name':linea['name'],'due':linea['due'],'member':linea['idMember'],'used':False}
+                                print(f"\t\t{linea['id']}\t{linea['name']}")
+                            self.checklist[trello_checklist['id']] = {"title" : trello_checklist['name'], 'urlCard':self.get_card_link(trello_checklist['idCard']), "element":list}
+                            #e in fine la salva
+
+                    else: # se è gia stata salvata
+
+                        for linea in trello_checklist['checkItems']:
+                            if linea['id'] in self.checklist[trello_checklist['id']]['element'].keys():
+                                if linea['idMember'] != self.checklist[trello_checklist['id']]['element'][linea['id']]['member']:
+                                    self.checklist[trello_checklist['id']]['element'][linea['id']]['used'] = False
+                            else:
+                                print(f"[NEW LINE]{linea['id']} {linea['name']}")
+                                self.checklist[trello_checklist['id']]['element'][linea['id']] = {'name':linea['name'],'due':linea['due'],'member':linea['idMember'],'used':False}
+                                self.checklist[trello_checklist['id']]['element'][linea['id']]['used'] = False
 
     def get_card_link(self,idCard):
         """ricava il link della card attraverso il codice"""
@@ -92,13 +119,13 @@ class WorkAssigment:
         for member in self.key['id_board'].keys():
             if self.key['id_board'][member] == "5ee869db1e066a18aec6ba98":
                 url = f"https://api.trello.com/1/boards/{self.key['id_board'][member]}/lists"
-                # print(f"member: {meber}")
+
                 response = requests.request(
                     "GET",
                     url,
                     params=self.query
                 )
-                # print(f"check_managment_list{response}")
+
 
                 if response.__str__() == "<Response [200]>":
                     list_name = [name['id'] for name in response.json() if name['name'] == "MANAGMEENT"]
@@ -118,37 +145,41 @@ class WorkAssigment:
                         )
 
                         if response.__str__() == "<Response [200]>":
-                            # print(f"ho creatmo managment {response.text}")
+
                             self.key['id_list'][member] = response.json()['id']
                         else:
                             print(f"check_managment_list c'è un problema {response.text}")
                     else:
-                        print(list_name)
                         self.key['id_list'][member] = list_name[0]
 
 
 
     def create_card(self):
-        i = 1
-        for key in self.checklist.keys():
-            for element in self.checklist[key]['element']:
-                if element['member'] != None:
-                    if element['member'] == "5ed8067e73403d50623f87fd":
-                        url = f"https://api.trello.com/1/cards"
-                        query = {
-                            'key': self.key['api_key'],
-                            'token': self.key['admin_token'],
-                            'idList': self.key['id_list'][element['member']],
-                            'name' : f"[{self.convert_date(element['due'])}] {element['name']}",
-                            'due' : element['due'],
-                        }
 
-                        response = requests.request(
-                            "POST",
-                            url,
-                            params=query
-                        )
-                        i = i+1
+        for key in self.checklist.keys():
+            for element in self.checklist[key]['element'].keys():
+                try:
+                    if self.checklist[key]['element'][element]['member'] != None:
+                        if self.checklist[key]['element'][element]['used'] == False:
+                            if self.checklist[key]['element'][element]['member'] == "5ed8067e73403d50623f87fd":
+                                url = f"https://api.trello.com/1/cards"
+                                query = {
+                                    'key': self.key['api_key'],
+                                    'token': self.key['admin_token'],
+                                    'idList': self.key['id_list'][self.checklist[key]['element'][element]['member']],
+                                    'name' : f"[{self.convert_date(self.checklist[key]['element'][element]['due'])}] {self.checklist[key]['element'][element]['name']}",
+                                    'due' : self.checklist[key]['element'][element]['due']
+                                }
+
+                                response = requests.request(
+                                    "POST",
+                                    url,
+                                    params=query
+                                )
+                                self.checklist[key]['element'][element]['used'] = True
+                except TypeError:
+                    print("non va bene")
+                    print(self.checklist[key]['element'])
 
     def convert_date(self,date):
         mesi = ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosti", "Settembre",
@@ -181,11 +212,70 @@ class WorkAssigment:
             )
 
             if response.__str__() == "<Response [200]>":
+                title_list = []
+                title_dict = {}
                 for card in response.json():
                     title = re.sub("[^\w]", " ", card['name']).split()
+                    title.append(card['id'])
                     try:
                         title[1] = mesi[title[1]]
+                        title = [word.__str__() for word in title]
+                        title = ' '.join(title)
 
-                        print()
+
                     except IndexError:
-                        pass
+                        title = ' '.join(title)
+
+                    except KeyError:
+                        title = ' '.join(title)
+
+                    title_dict[title] = card['id']
+                    title_list.append(title)
+
+                title_list = sorted(title_list)
+
+                i = 0
+                for i in range(0,len(title_list),1):
+                    url = f"https://api.trello.com/1/cards/{title_dict[title_list[i]]}"
+
+                    headers = {
+                        "Accept": "application/json"
+                    }
+
+                    query = {
+                        'key': self.key['api_key'],
+                        'token': self.key['admin_token'],
+                        'pos' : i+1
+                    }
+
+                    response = requests.request(
+                        "PUT",
+                        url,
+                        headers=headers,
+                        params=query
+                    )
+
+
+
+    def chek_card_status(self,id_card):
+        url = f"https://api.trello.com/1/cards/{id_card}"
+
+        headers = {
+            "Accept": "application/json"
+        }
+
+        query = {
+            'key': self.key['api_key'],
+            'token': self.key['admin_token']
+        }
+
+        response = requests.request(
+            "GET",
+            url,
+            headers=headers,
+            params=query
+        )
+
+        if response.__str__() == "<Response [200]>":
+
+            return response.json()['closed']
